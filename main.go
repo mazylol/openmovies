2344282719cc095de19afabe656a975bb502d24a
mazylol <landonp95@gmail.com>
2023-09-17 18:29:51 -0400

    reverse proxy no longer needed

--- main.go
+++ main.go
@@ -7,38 +7,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/mazylol/openmovies/types"
 	"log"
-	"net/http"
-	"net/http/httputil"
-	"net/url"
 	"os"
 	"strings"
 )
 
 var movies = make(map[string]types.Movie)
 
-func proxy(c *gin.Context) {
-	remote, err := url.Parse("https://mazylol.com")
-	if err != nil {
-		panic(err)
-	}
-
-	proxy := httputil.NewSingleHostReverseProxy(remote)
-	proxy.Director = func(req *http.Request) {
-		req.Header = c.Request.Header
-		req.Host = remote.Host
-		req.URL.Scheme = remote.Scheme
-		req.URL.Host = remote.Host
-		req.URL.Path = c.Request.URL.Path
-	}
-
-	proxy.ServeHTTP(c.Writer, c.Request)
-
-}
-
 func main() {
 	r := gin.Default()
-
-	r.NoRoute(proxy)
 
 	r.Use(cors.Default())
 
